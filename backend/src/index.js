7fec7931ab26f8ea3314e417fd503aa11d552292
Akash <Akash.patel.ap88020@gmail.com>
2025-05-28 19:11:06 +0530

    setup database or created router file for user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import connectDB from './lib/db.js';
 
 const app = express();
-
 const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`app is listening at port ${port}`);
+// app.use('/api/user/');
+// app.use('/api/capsule/');
+
+connectDB().then(() => {
+    app.listen(port,() => {
+        console.log(`app is listening at prt ${port}`);
+    })
 })
